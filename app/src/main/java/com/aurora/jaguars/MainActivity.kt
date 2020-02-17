dab24192114a74ba4d3e91ed35a3eb863085b217
Leon Si <leonzalion@gmail.com>
2020-02-17 14:58:59 -0500

    d

--- app/src/main/java/com/aurora/jaguars/MainActivity.kt
+++ app/src/main/java/com/aurora/jaguars/MainActivity.kt
@@ -49,18 +49,22 @@ class MainActivity : AppCompatActivity() {
         var btn_reset = findViewById(R.id.btn_reset) as Button
         var btn_submit = findViewById(R.id.btn_submit) as Button
 
+        /*
         btn_reset.setOnClickListener {
             // clearing user_name and password edit text views on reset button click
             et_user_name.setText("")
             et_password.setText("")
         }
+        */
 
+        /*
         // set on-click listener
         btn_submit.setOnClickListener {
             val user_name = et_user_name.text;
             val password = et_password.text;
             Toast.makeText(this@MainActivity, user_name, Toast.LENGTH_LONG).show()
         }
+        */
 
 
 
